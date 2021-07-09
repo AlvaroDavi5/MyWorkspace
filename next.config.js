9154bab633a2d988bf0a62b41f07daa85deacd91
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-07-09 04:34:49 -0300

    pwa and image from free dir not working

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = withPlugins([
 		withPwa,
 		{
 			pwa: {
-				disable: process.env.NODE_ENV != 'production',
+				disable: process.env.NODE_ENV === 'development',
 				dest: 'public',
 				register: true,
 				sw: '/sw.js'
