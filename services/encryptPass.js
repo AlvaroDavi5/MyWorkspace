a68cb81aedaa7f657b326913bd8fd146a1af7936
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2021-09-13 23:05:38 -0300

    finished controllers services and password encrypt

---
+++ services/encryptPass.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv')
+dotenv.config({path:__dirname+"/../.env.development.local"})
+const crypto = require('crypto')
+const cipher = crypto.createCipher(process.env.CRYPTO_ALGORITHM, process.env.CRYPTO_KEY)
+
+
+export default function encrypt(password) {
+	cipher.update(password)
+
+	const pass = cipher.final(process.env.CRYPTO_METHOD)
+
+	return pass
+}
--- services/userController.js
+++ services/userController.js
@@ -1,17 +1,19 @@
-const path = require('path')
-const dotenv = require('dotenv')
-dotenv.config({path:__dirname+"/../.env.development.local"})
-const crypto = require('crypto')
-const cipher = crypto.createCipher(process.env.CRYPTO_ALGORITHM, process.env.CRYPTO_KEY)
-
-
+import encrypt from "./encryptPass.js"
 const connection = require("../database/connection.js")
 const Users = require("../database/models/users.js")
+const UserPreferences = require("../database/models/user_preferences.js")
 
+
+/*
+ * CRUD database operations
+  ? Create
+  ? Read
+  ? Update
+  ? Delete
+*/
 async function createUser(name, email, password, phone, cpf, uf, returnId) {
 	Users.init(connection)
-	cipher.update(password)
-	const pass = cipher.final(process.env.CRYPTO_METHOD)
+	const pass = encrypt(password)
 
 	try {
 		const user = await Users.create(
@@ -26,7 +28,7 @@ async function createUser(name, email, password, phone, cpf, uf, returnId) {
 		)
 
 		if (returnId == true) {
-			return getUserIdByName(name)
+			return user.id
 		}
 		else {
 			return true
@@ -67,5 +69,123 @@ async function getUserIdByName(userName) {
 	}
 }
 
+async function updateUser(user, name, email, password, phone, cpf, uf) {
+	Users.init(connection)
 
-export { createUser, getUserById, getUserIdByName }
+	try {
+		if (name) { user.name = name }
+		if (email) { user.email = email }
+		if (password) { user.password = encrypt(password) }
+		if (phone) { user.phone = phone }
+		if (cpf) { user.cpf = cpf }
+		if (uf) { user.uf = uf }
+
+		await user.save()
+
+		return true
+	}
+	catch ({ message }) {
+		return false
+	}
+}
+
+async function deleteUser(user) {
+	Users.init(connection)
+
+	try {
+		await user.destroy()
+
+		return true
+	}
+	catch ({ message }) {
+		return false
+	}
+}
+
+async function createPreference(user_id, image_path, default_theme, returnId) {
+	UserPreferences.init(connection)
+
+	try {
+		const preference = await UserPreferences.create(
+			{
+				user_id: user_id,
+				image_path: image_path,
+				default_theme: default_theme
+			}
+		)
+
+		if (returnId == true) {
+			return preference.id
+		}
+		else {
+			return true
+		}
+	}
+	catch ({ message }) {
+		return false
+	}
+}
+
+async function getPreferenceById(id) {
+	UserPreferences.init(connection)
+
+	try {
+		const preference = await UserPreferences.findByPk(id)
+
+		return preference
+	}
+	catch ({ message }) {
+		return null
+	}
+}
+
+async function getPreferenceIdByUserId(user_id) {
+	UserPreferences.init(connection)
+
+	try {
+		const preference = await UserPreferences.findOne({
+			where: {
+				user_id: user_id
+			}
+		})
+
+		return preference.id
+	}
+	catch ({ message }) {
+		return null
+	}
+}
+
+async function updatePreference(preference, user_id, image_path, default_theme) {
+	UserPreferences.init(connection)
+
+	try {
+		if (user_id) { preference.user_id = user_id }
+		if (image_path) { preference.image_path = image_path }
+		if (default_theme) { preference.default_theme = default_theme }
+
+		await preference.save()
+
+		return true
+	}
+	catch ({ message }) {
+		return false
+	}
+}
+
+async function deletePreference(preference) {
+	UserPreferences.init(connection)
+
+	try {
+		await preference.destroy()
+
+		return true
+	}
+	catch ({ message }) {
+		return false
+	}
+}
+
+
+export { createUser, getUserById, getUserIdByName, updateUser, deleteUser,
+createPreference, getPreferenceById, getPreferenceIdByUserId, updatePreference, deletePreference }
