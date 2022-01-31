9754a6cd1455df15e68afe77d5b29df7069de255
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-01-31 15:25:02 -0300

    fix/up: updated project dependencies
    and fixed connection bug

--- database/connection.js
+++ database/connection.js
@@ -11,7 +11,7 @@ const connection = new Sequelize(DBConfig)
 
 /* testing the connection */
 try {
-	await connection.authenticate()
+	connection.authenticate()
 	console.log('Database connection has been established successfully.')
 }
 catch (error) {
